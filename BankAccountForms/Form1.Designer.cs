61ede6acc43934fb6811eefb829cec47cce1fa26
arcum-omni <travis.l.eiler@gmail.com>
2020-04-22 08:23:34 -0700

    Created simple account creation form

--- BankAccountForms/Form1.Designer.cs
+++ BankAccountForms/Form1.Designer.cs
@@ -28,13 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.initialBalanceTxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // initialBalanceTxt
+            // 
+            this.initialBalanceTxt.Location = new System.Drawing.Point(221, 104);
+            this.initialBalanceTxt.Name = "initialBalanceTxt";
+            this.initialBalanceTxt.Size = new System.Drawing.Size(191, 26);
+            this.initialBalanceTxt.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(88, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Initial Balance:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(161, 169);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 77);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Create Account";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(507, 376);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.initialBalanceTxt);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox initialBalanceTxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
